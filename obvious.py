2ba527306d052ce5d8497c4afe75d4c673880552
deisner <deisner@bionym.com>
2017-06-13 10:30:03 -0400

    python -- more path stuff, 2/3 input, peer repo management

--- obvious.py
+++ obvious.py
@@ -1,3 +1,19 @@
+#=====Python 2/3 compatibility=====#
+try: input=raw_input
+except: pass
+
+#=====peer repo management=====#
+def ensure_repo(path, name):
+	import os, subprocess
+	if not os.path.exists(os.path.join(path, name)):
+		start=os.getcwd()
+		os.chdir(path)
+		print("I need to clone {} repo. I'll do so in {}. Is this OK? (y/n)".format(name, os.getcwd()))
+		if input()!='y': raise Exception('user disallowed {} clone'.format(name))
+		subprocess.check_call('git clone '.format(url), shell=True)
+		os.chdir(start)
+
+#=====Python 2/3 path stuff=====#
 import os
 
 class Path:
@@ -21,15 +37,23 @@ class Path:
 						assert not hasattr(self, 'p')
 						self.p=x
 		else: self.p=os.getcwd()
+		if hasattr(self, 'p'): self.p=os.path.realpath(self.p)
 
 	def __nonzero__(self):
 		if not hasattr(self, 'p'): return False
 		return os.path.exists(self.p)
+
 	def __bool__(self): return self.__nonzero__()
 
 	def __str__(self): return self.p
+
 	def __add__(self, s): return self.join(s)
+
 	def join(self, s): return Path(os.path.join(self.p, Path(s).p))
+
 	def cd(self): os.chdir(self.p)
-	def real(self): return os.path.realpath(self.p)
-	def parent(self): return Path(os.path.split(self.p)[0])
+
+	def parent(self, n=1):
+		r=self.p
+		for i in range(n): r=os.path.split(r)[0]
+		return r
